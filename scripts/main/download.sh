ba0388596f26f998fd6939edfa9cd6cafa86a380
awd <git@awdawd.eu>
2025-06-16 18:43:15 +0000

    change directory to variable

--- scripts/main/download.sh
+++ scripts/main/download.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
 
+dir=/opt/ainstall
+
 sudo pacman -Sy --noconfirm --needed git
-git clone https://git.awdawd.eu/awd/ainstall /opt/ainstall
-cd /opt/ainstall
+git clone https://git.awdawd.eu/awd/ainstall ${dir}
+cd ${dir}
 
 ./ainstall -S installArch -i sda
