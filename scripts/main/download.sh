f1e2c23afea5843bb76f92844b76d4686ce130d7
awd <git@awdawd.eu>
2025-06-16 18:43:35 +0000

    delete directory if exists

--- scripts/main/download.sh
+++ scripts/main/download.sh
@@ -3,6 +3,11 @@ set -e
 
 dir=/opt/ainstall
 
+if [[ -e ${dir} ]]
+then
+  rm -r ${dir}
+fi
+
 sudo pacman -Sy --noconfirm --needed git
 git clone https://git.awdawd.eu/awd/ainstall ${dir}
 cd ${dir}
