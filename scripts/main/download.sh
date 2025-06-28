37f2919adbf3a6363dfd4fbc4dd90e8438ed8241
awd <git@awdawd.eu>
2025-06-28 17:22:53 +0000

    add download logging

--- scripts/main/download.sh
+++ scripts/main/download.sh
@@ -12,4 +12,17 @@ sudo pacman -Sy --noconfirm --needed git
 git clone https://git.awdawd.eu/awd/ainstall ${dir}
 cd ${dir}
 
+if [ ! -d ${dir}/logs ]
+then
+  mkdir -p ${dir}/logs
+fi
+
+touch ${dir}/logs/download.log
+
+cat > ${dir}/logs/download.log <<EOF
+downloaded: $(date)
+
+dir: ${dir}
+EOF
+
 ./ainstall -S installArch -i sda
